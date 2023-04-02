d9c30e5cf5c5a82c6e92f69bbeb1a6192868ecca
aecsocket <aecsocket@tutanota.com>
2023-04-02 21:03:26 +0100

    FAffine3 API

--- src/main/kotlin/io/github/aecsocket/klam/Affine.kt
+++ src/main/kotlin/io/github/aecsocket/klam/Affine.kt
@@ -2,6 +2,10 @@
 
 package io.github.aecsocket.klam
 
+inline fun inverse(t: FAffine3): FAffine3 {
+    val rotInv = inverse(t.rotation)
+    return FAffine3(rotInv * -t.position, rotInv)
+}
 inline fun inverse(t: DAffine3): DAffine3 {
     val rotInv = inverse(t.rotation)
     return DAffine3(rotInv * -t.position, rotInv)
