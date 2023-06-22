22b7a6d625369356543bcdb12264ccfc2061ff38
aecsocket <aecsocket@tutanota.com>
2023-06-22 18:35:06 +0100

    0.2.4-SNAPSHOT: RGBA changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.aecsocket"
-version = "0.2.3"
+version = "0.2.4-SNAPSHOT"
 description = "Linear algebra library for 2D/3D applications"
 
 templates {
--- src/main/kotlin/io/github/aecsocket/klam/Color.kt
+++ src/main/kotlin/io/github/aecsocket/klam/Color.kt
@@ -5,28 +5,30 @@ typealias RGBA = IVec4
 
 //    RR GG BB AA
 // 0x ff ff ff ff
-fun asRGBA(v: RGBA) = ((v.r and 0xff) shl 24) or
-        ((v.g and 0xff) shl 16) or
-        ((v.b and 0xff) shl 8) or
-        ((v.a and 0xff) shl 0)
+fun rgbaToInt(v: RGBA) =
+    ((v.r and 0xff) shl 24) or
+    ((v.g and 0xff) shl 16) or
+    ((v.b and 0xff) shl  8) or
+    ((v.a and 0xff) shl  0)
 
-fun fromRGBA(i: Int) = RGBA(
+fun intToRGBA(i: Int) = RGBA(
     (i shr 24) and 0xff,
     (i shr 16) and 0xff,
-    (i shr 8) and 0xff,
-    (i shr 0) and 0xff,
+    (i shr  8) and 0xff,
+    (i shr  0) and 0xff,
 )
 
 //    AA RR GG BB
 // 0x ff ff ff ff
-fun asARGB(v: ARGB) = ((v.a and 0xff) shl 24) or
-        ((v.r and 0xff) shl 16) or
-        ((v.g and 0xff) shl 8) or
-        ((v.b and 0xff) shl 0)
+fun argbToInt(v: ARGB) =
+    ((v.a and 0xff) shl 24) or
+    ((v.r and 0xff) shl 16) or
+    ((v.g and 0xff) shl  8) or
+    ((v.b and 0xff) shl  0)
 
-fun fromARGB(i: Int) = ARGB(
+fun intToARGB(i: Int) = ARGB(
     (i shr 16) and 0xff,
-    (i shr 8) and 0xff,
-    (i shr 0) and 0xff,
+    (i shr  8) and 0xff,
+    (i shr  0) and 0xff,
     (i shr 24) and 0xff,
 )
